0b47402ad83aab3da5f935db337fc79bead802ee
tanzilgit <tanzilworld@gmail.com>
2022-03-13 13:31:07 +0530

    Update index.js

--- components/Test/index.js
+++ components/Test/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState, React } from "react";
+import styled from "styled-components";
 
-function index() {
+
+const Button = styled.button`
+  background: ${(props) => (props?.active ? "red" : "yellow")};
+  padding: 50px;
+`;
+
+function Index() {
+  // const [buttonClicked, setButtonClicked] = useState(false);
+
+  // const handleClick = () => setButtonClicked(!buttonClicked);
   return (
-    <div>Test Component</div>
-  )
+    // <div>Test Component</div>
+    <>
+      <Button
+        className="button"
+        // onClick={handleClick}
+        // active= {buttonClicked}
+      >
+        {/* {buttonClicked ? "First" : "Second"} */}
+        test
+      </Button>
+    </>
+  );
 }
 
-export default index
+export default Index;
