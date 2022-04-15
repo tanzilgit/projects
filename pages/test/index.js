8418ba74bd3e73516184bc1b92161f21bc0860d8
tanzilgit <tanzilworld@gmail.com>
2022-04-15 23:30:11 +0530

    Create index.js

---
+++ pages/test/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const ParentContainer = styled.div`
+  .normal {
+    font-size: 20px;
+    padding: 20px;
+    background-color: red;
+  }
+  .activeClass {
+    background-color: green;
+  }
+`;
+
+const Tab = styled.button`
+  font-size: 20px;
+  padding: 20px;
+  background: ${(props) => (props.styleActiveProp ? "green" : "red")};
+`;
+
+const Test = () => {
+  const [active, setActive] = useState("first");
+
+  const [classState, setClassState] = useState("one");
+  return (
+    <ParentContainer>
+      <h1>Using Styled Component props </h1>
+      <Tab
+        styleActiveProp={active === "first" ? 1 : 0}
+        onClick={() => setActive("first")}
+      >
+        First
+      </Tab>
+      <Tab
+        styleActiveProp={active === "second" ? 1 : 0}
+        onClick={() => setActive("second")}
+      >
+        Second
+      </Tab>
+      <h1>Using Class : Adding CSS under Parent Styled Component </h1>
+      <button className={classState === "one" ? "activeClass normal" : "normal"} onClick={()=>setClassState("one") }>
+        One
+      </button>
+      <button className={classState === "two" ? "activeClass normal" : "normal"} onClick={()=>setClassState("two") }>
+        Two
+      </button>
+    </ParentContainer>
+  );
+};
+
+export default Test;
