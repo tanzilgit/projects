b2d1443a0e677d5d99ad7aec5fcab3ddb902c22f
tanzilgit <tanzilworld@gmail.com>
2025-07-06 03:03:19 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import MegaMenu from "../components/MegaMenu";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
 import "../styles/mega.css";
+import "../styles/crm.css";
 import SimpleAccordion from "../components/Accordion";
 import BackToTop from "../components/BackToTop";
 
